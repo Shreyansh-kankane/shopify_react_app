e1baed4f8dd30b087a4f645122a198faf9643fa4
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2025-01-17 13:27:51 +0530

    with app bridge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {useAppBridge} from '@shopify/app-bridge-react';
 
 function App() {
-    return <h1>Hello World 2</h1>;
+    const shopify = useAppBridge();
+
+    function generateBlogPost() {
+        // Handle generating
+        shopify.toast.show('Blog post template generated');
+      }
+    
+    return (
+        <>
+            <button onClick={generateBlogPost}>Generate Blog Post</button>;
+            <h1>Hello World 2</h1>;
+        </>
+    )
+
 }
 
 export default App;
